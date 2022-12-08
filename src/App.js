eda2144c7758e26b90aead8b2eddf73d7b692397
md.rahman <md.rahman@mdrahmans-MacBook-Pro.local>
2022-12-08 08:21:29 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// function or class
+// return UI or null
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return <div>
+        <h1>Hello React!!</h1>
     </div>
-  );
-}
+};
 
 export default App;
