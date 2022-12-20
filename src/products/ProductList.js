c7338fe17108aad1b70b8491bc182a1e607bed3a
Mujib <mujib517@gmail.com>
2022-12-20 08:39:03 +0530

    migrate productList component to use hooks

--- src/products/ProductList.js
+++ src/products/ProductList.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductItem from './Product';
 import Error from '../common/Error';
 import ShouldRender from '../common/ShouldRender';
 import { Link } from 'react-router-dom';
 
-class ProductList extends Component {
+const ProductList = () => {
+    const [response, setResponse] = useState({
+        metadata: {},
+        data: []
+    });
+    const [hasError, setError] = useState(false);
 
-    state = {
-        response: {
-            metadata: {},
-            data: []
-        },
-        hasError: false
-    }
-
-    constructor() {
-        super();
-
+    useEffect(() => {
         axios.get('https://fsa-api-b4.onrender.com/api/products/page/1/limit/10')
-            .then(res => this.setState({ response: res.data, hasError: false }))
-            .catch(err => this.setState({ hasError: true }));
-    }
+            .then(res => {
+                setError(false);
+                setResponse(res.data);
+            })
+            .catch(err => setError(true));
+    }, []);
 
-    render() {
-        return <div className="container">
-            <h4>Products</h4>
-            <Link className="btn btn-danger btn-sm"
-                to="/products/new">
-                Add Product
-            </Link>
-            <ShouldRender cond={this.state.hasError}>
-                <Error />
-            </ShouldRender>
-            {
-                this.state.response.data.map(
-                    product => <ProductItem product={product} />
-                )
-            }
-        </div>;
-    }
+    return <div className="container">
+        <h4>Products</h4>
+        <Link className="btn btn-danger btn-sm"
+            to="/products/new">
+            Add Product
+        </Link>
+        <ShouldRender cond={hasError}>
+            <Error />
+        </ShouldRender>
+        {
+            response.data.map(
+                product => <ProductItem product={product} />
+            )
+        }
+    </div>;
 }
 
 export default ProductList;
