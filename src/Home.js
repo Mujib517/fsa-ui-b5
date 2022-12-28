6f7bb94544a478eddc708b3d28d3dc696fdb0305
Mujib <mujib517@gmail.com>
2022-12-28 08:45:52 +0530

    modify home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AutoCounter from './AutoCounter';
 
 const Home = () => <div className="container">
-    <h1>Home Page</h1>
+    <h1>Home Page!!</h1>
     <AutoCounter />
 </div>
 
